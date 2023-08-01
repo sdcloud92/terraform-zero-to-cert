18784e89fc11b904062b8e5a87195379a0a09ae1
sdcloud92 <shomarri.diaz@gmail.com>
2023-08-01 02:15:28 +0100

    using outputs to reveal execution results

--- lab6-execution-results-outputs/outputs.tf
+++ lab6-execution-results-outputs/outputs.tf
@@ -1,1 +1,17 @@
+output "web_instance_public_ip" {
+  value = aws_instance.web_instance.public_ip
+}
+
+output "app_instance_public_ip" {
+  value = aws_instance.app_instance.public_ip
+}
+
+output "db_instance_public_ip" {
+  value = aws_instance.db_instance.public_ip
+}
+
+output "security_group_id" {
+  value = aws_security_group.instance_sg.id
+}
+
 
