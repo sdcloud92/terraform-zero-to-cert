a37b227334763f98b581ac2077ed07a3fbe46454
sdcloud92 <shomarri.diaz@gmail.com>
2023-08-03 22:37:33 +0100

    first

--- lab10-Data-Source-To-Find-AMI/main.tf
+++ lab10-Data-Source-To-Find-AMI/main.tf
@@ -1,1 +1,37 @@
+# resource "aws_instance" "myserver" {
+#     ami = "data.aws_ami.latest_ubuntu20.id"
+#     instance_type = "t2.micro"
+# }
+
+data "aws_ami" "latest_ubuntu_20" {
+  owners      = ["099720109477"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"] #Swapped out timestamp for *
+  }
+}
+
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["137112412989"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*.0-kernel-6.1-x86_64"] #Swapped out timestamp for *
+  }
+}
+
+data "aws_ami" "latest_windows_2019" {
+  owners      = ["801119661308"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2019-English-Full-Base-*"] #Swapped out timestamp for *
+  }
+}
+
+
+
+
+
 
